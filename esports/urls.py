18cd959f32be7b22109dc16413cc1693f2ebff8a
atul <atulg0736@gmail.com>
2024-12-19 22:16:55 +0530

    logo

--- esports/urls.py
+++ esports/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler404, handler500
 from home.views import error_404_view, error_500_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 handler404 = error_404_view
 handler500 = error_500_view
@@ -13,3 +15,5 @@ urlpatterns = [
     path('', include('home.urls')),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
